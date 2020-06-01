487d269d27ec0c7a23a033c3df52de5e0b5bf8ff
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-02 08:40:02 +1200

    adding pipe and mapping the data from observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable, Subscription} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -22,13 +23,29 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        // checking error of observable to check observable.error
+        // if (count > 2) {
+        //   observer.complete();
+        // }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    });
+
+    this.firstObsSubscription = customIntervalObservable.pipe(map((data: number) => `Round is ${data + 1}`))
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+        alert(error);
+      }, () => {
+        console.log('observable completed');
+
+      });
 
   }
 
